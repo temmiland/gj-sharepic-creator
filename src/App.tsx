d40e5d2e311c05f5f0b9d9ac164b378af81e142f
Temmi Pietsch <welcome@temmi.land>
2025-03-08 18:03:05 +0100

    🚑️ fixed overflow

--- src/App.tsx
+++ src/App.tsx
@@ -279,7 +279,7 @@ function App() {
           <input type='number' value={pictogramPosition.x} onChange={(e) => setPictogramPosition({ ...pictogramPosition, x: parseInt(e.target.value) })} />
           <input type='number' value={pictogramPosition.y} onChange={(e) => setPictogramPosition({ ...pictogramPosition, y: parseInt(e.target.value) })} />
         </div>
-		<p>v0.0.1 - build 08.03.2025 - <a href="https://github.com/temmiland/gj-sharepic-generator">github</a></p>
+		<p>v0.0.2 - build 08.03.2025 - <a href="https://github.com/temmiland/gj-sharepic-generator">github</a></p>
       </div>
     </div>
   );
